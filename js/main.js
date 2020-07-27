25079fa22e8a6d618351679974f67d40b96a0d70
shalabycr7 <cr7.shalaby@gmail.com>
2020-07-27 13:24:35 +0200

    p

--- js/main.js
+++ js/main.js
@@ -289,41 +289,41 @@ function getTime(classN) {
 function profPageLoad() {
   var profCloseBu = document.getElementById('closeProf');
   var f = document.getElementById('profForm');
-  var j=document.getElementById('ch');
-  
+  var j = document.getElementById('ch');
+
   profCloseBu.addEventListener('click', function() {
     var name = f['profName'].value;
     var email = f['profEmail'].value;
     var phone = f['profPhone'].value;
-    if (name==''||email==''||phone=='') {
-    
+    if (name == '' || email == '' || phone == '') {
+
     } else {
       localStorage.setItem('nn', name);
-    localStorage.setItem('ee', email);
-    localStorage.setItem('pp', phone);
-    location.href = './contacts.html';
+      localStorage.setItem('ee', email);
+      localStorage.setItem('pp', phone);
+      location.href = './contacts.html';
 
-    
+
     }
   })
   f[0].value = pName;
   f[1].value = pEmail;
   f[2].value = pPhone;
-  
-  j.addEventListener('change',function(){
-    var k=j.checked.toString();
-    localStorage.setItem('check',k)
-    
+
+  j.addEventListener('change', function() {
+    var k = j.checked.toString();
+    localStorage.setItem('check', k)
+
   })
-  
-  if (kk=='true'){
-    j.checked=true;
-    location.href='./contacts.html'
+
+  if (kk == 'true') {
+    j.checked = true;
+
+    document.body.innerHTML = '';
+    location.href = './contacts.html'
   }
 }
 var pName = localStorage.getItem('nn');
 var pEmail = localStorage.getItem('ee');
 var pPhone = localStorage.getItem('pp');
-var kk=localStorage.getItem('check');
-
-function cg(){alert('hpppp')}
+var kk = localStorage.getItem('check');
